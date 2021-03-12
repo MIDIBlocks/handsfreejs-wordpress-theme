bb260b685c7f670cbaf95ba5f58e8458c40bdf9f
Oz Ramos <midiblocks@gmail.com>
2021-03-12 12:01:47 -0800

    Adds admin styles and moves customizer styles into stylesheet

--- functions.php
+++ functions.php
@@ -3,3 +3,11 @@
 
 include get_stylesheet_directory() . '/cpt/plugin.php';
 include get_stylesheet_directory() . '/shortcodes/global.php';
+
+
+/**
+ * Admin styles
+ */
+add_action('admin_enqueue_scripts', function ($hook) {
+  wp_enqueue_style('admin_styles', get_stylesheet_directory_uri() . '/style-admin.css');
+});
