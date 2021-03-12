1d32613e8320027e1c08f837fa0c05b6ed7d0328
Oz Ramos <midiblocks@gmail.com>
2021-03-12 13:38:09 -0800

    Removes extra twitter widget error catchers

--- js/global.js
+++ js/global.js
@@ -17,30 +17,9 @@ function checkForTwitterErrors () {
   if (!window.twttr) {
     setTimeout(() => {
       checkForTwitterErrors()
-    }, 50)
+    }, 250)
   } else {
     twttr.ready(function (twttr) {
-      twttr.events.bind('loaded', function () {
-        console.clear()
-        setTimeout(() => {
-          console.clear()
-        }, 50)
-        setTimeout(() => {
-          console.clear()
-        }, 100)
-        setTimeout(() => {
-          console.clear()
-        }, 250)
-        setTimeout(() => {
-          console.clear()
-        }, 500)
-        setTimeout(() => {
-          console.clear()
-        }, 750)
-        setTimeout(() => {
-          console.clear()
-        }, 1000)
-      })
       twttr.events.bind('rendered', function () {
         console.clear()
         setTimeout(() => {
