c50a98e4b5d169159e477a717239638912f1a631
Oz Ramos <pixelfelt@gmail.com>
2021-04-15 11:52:09 -0700

    #5 Updates ACF with user fields, starts [author-meta] w/ photo & text

--- shortcodes/global.php
+++ shortcodes/global.php
@@ -8,3 +8,22 @@ add_shortcode('title', function ($atts) {
   global $post;
   return $post->post_title;
 });
+
+/**
+ * [author-meta]
+ * 
+ * Displays the posts author, name, socials, and more
+ */
+add_shortcode('author-meta', function () {
+  global $post;
+
+  ob_start(); ?>
+    <div class="author-meta">
+      <?= get_avatar(get_the_author_meta('ID'), 128) ?>
+      <div class="author-meta-details">
+        <h3><?= get_the_author_meta('display_name') ?></h3>
+      </div>
+      <p><?= get_the_author_meta('description') ?></p>
+    </div>
+  <?php return ob_get_clean();
+});
