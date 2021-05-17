a83f46f5518a7a1d54b5f729d5bf65bd9679f2b4
Oz Ramos <pixelfelt@gmail.com>
2021-05-17 11:43:28 -0700

    #14 Gets the demo working

--- shortcodes/demo/aframe-look-around/index.php
+++ shortcodes/demo/aframe-look-around/index.php
@@ -3,7 +3,9 @@
  * [demo-aframe-look-around]
  */
 add_shortcode('demo-aframe-look-around', function () {
+  wp_enqueue_script('demo-aframe-look-around', get_stylesheet_directory_uri() . '/shortcodes/demo/aframe-look-around/demo.js', ['global'], null, true);
+
   ob_start(); ?>
-    <iframe src="<?= get_stylesheet_directory_uri() ?>/shortcodes/demo/aframe-look-around/demo.php" style="width: 100%; height: 400px"></iframe>
+    <iframe class="demo-aframe-look-around" src="<?= get_stylesheet_directory_uri() ?>/shortcodes/demo/aframe-look-around/demo.php" style="width: 100%; height: 400px"></iframe>
   <?php return ob_get_clean();
 });
